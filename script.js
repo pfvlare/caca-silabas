796f98bb1e6f1278ec2e0f5413765fcac5efd93d
Larissa Campos <108104493+pfvlare@users.noreply.github.com>
2024-06-07 20:20:47 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,201 +1,276 @@
-// Obtenção dos elementos do HTML
-const wordImage = document.getElementById('word-image'); // Elemento de imagem da palavra
-const syllablesContainer = document.getElementById('syllables-container'); // Contêiner das sílabas
-const correctCountSpan = document.getElementById('correct-count'); // Contador de acertos
-const wrongCountSpan = document.getElementById('wrong-count'); // Contador de erros
-const resultDiv = document.getElementById('result'); // Div para exibir resultados
-const selectedSyllablesDiv = document.getElementById('selected-syllables'); // Div para exibir sílabas selecionadas
+const wordImage = document.getElementById('word-image');
+const syllablesContainer = document.getElementById('syllables-container');
+const correctCountSpan = document.getElementById('correct-count');
+const wrongCountSpan = document.getElementById('wrong-count');
+const resultDiv = document.getElementById('result');
+const selectedSyllablesDiv = document.getElementById('selected-syllables');
 
-// Palavras do jogo e suas sílabas
 const words = [
-    { image: 'bolo.png', syllables: ['bo', 'lo'] },
-    { image: 'casa.jpg', syllables: ['ca', 'sa'] },
-    { image: 'bule.jpg', syllables: ['bu', 'le'] },
-    { image: 'cubo.jpg', syllables: ['cu', 'bo'] },
-    { image: 'dado.jpg', syllables: ['da', 'do'] },
-    { image: 'dedo.png', syllables: ['de', 'do'] },
-    { image: 'foca.png', syllables: ['fo', 'ca'] },
-    { image: 'fone.png', syllables: ['fo', 'ne'] },
-    { image: 'lapis.jpg', syllables: ['la', 'pis'] },
-    { image: 'lula.jpg', syllables: ['lu', 'la'] },
-    { image: 'gato.png', syllables: ['ga', 'to'] },
-    // Adicione mais palavras e suas sílabas aqui
+    { image: 'bolo.png', syllables: ['BO', 'LO'] },
+    { image: 'casa.jpg', syllables: ['CA', 'SA'] },
+    { image: 'bule.jpg', syllables: ['BU', 'LE'] },
+    { image: 'cubo.jpg', syllables: ['CU', 'BO'] },
+    { image: 'dado.jpg', syllables: ['DA', 'DO'] },
+    { image: 'dedo.png', syllables: ['DE', 'DO'] },
+    { image: 'foca.png', syllables: ['FO', 'CA'] },
+    { image: 'fone.png', syllables: ['FO', 'NE'] },
+    { image: 'lapis.jpg', syllables: ['LA', 'PIS'] },
+    { image: 'lula.jpg', syllables: ['LU', 'LA'] },
+    { image: 'gato.png', syllables: ['GA', 'TO'] },
 ];
 
-let currentWordIndex = 0; // Índice da palavra atual
-let currentWord = words[currentWordIndex]; // Palavra atual
-let selectedSyllables = []; // Array para armazenar as sílabas selecionadas
-let correctCount = 0; // Contador de acertos
-let wrongCount = 0; // Contador de erros
-let userName = ''; // Nome do usuário
-let userAge = ''; // Idade do usuário
+const wordsPhaseTwo = [
+    { image: 'elefante.jpg', syllables: ['E', 'LE', 'FAN', 'TE'] },
+    { image: 'jacaré-38125377.webp', syllables: ['JA', 'CA', 'RE'] },
+    { image: 'abacaxi.WEBP', syllables: ['A', 'BA', 'CA', 'XI'] },
+    { image: 'macaco.jpg', syllables: ['MA', 'CA', 'CO'] },
+    { image: 'BONECA.jpg', syllables: ['BO', 'NE', 'CA'] },
+    { image: 'AMIGOS.PNG', syllables: ['A', 'MI', 'GOS'] },
+    { image: 'BICICLETA.jpg', syllables: ['BI', 'CI', 'CLE','TA'] },
+    { image: 'GIRAF.PNG', syllables: ['GI', 'RA', 'FA'] },
+    { image: 'chapeuF.JPG', syllables: ['CHA', 'PEU', ] },
+    { image: 'carro.JPG', syllables: ['CAR', 'RO'] },
+    { image: 'AVIAO.JPG', syllables: ['A', 'VI', 'AO'] },
 
-// Função para gerar sílabas com vogais
+   
+    
+];
+
+let currentWordIndex = 0;
+let currentWord = words[currentWordIndex];
+let selectedSyllables = [];
+let correctCount = 0;
+let wrongCount = 0;
+let userName = '';
+let userAge = '';
+let isPhaseTwo = false;
+let timer;
+let timeLimit = 15;
+
+function shuffleArray(array) {
+    for (let i = array.length - 1; i > 0; i--) {
+        const j = Math.floor(Math.random() * (i + 1));
+        [array[i], array[j]] = [array[j], array[i]];
+    }
+}
+
 function generateSyllablesWithVowels(syllable) {
-    const vowels = ['a', 'e', 'i', 'o', 'u'];
+    const vowels = ['A', 'E', 'I', 'O', 'U'];
     const syllablesWithVowels = [];
 
     vowels.forEach(vowel => {
-        syllablesWithVowels.push(syllable.replace(/[aeiou]/g, vowel));
+        syllablesWithVowels.push(syllable.replace(/[aeiou]/gi, vowel));
     });
 
     return syllablesWithVowels;
 }
 
-// Função para exibir a palavra atual
 function displayWord() {
-    wordImage.src = currentWord.image; // Define a imagem da palavra
+    wordImage.src = currentWord.image;
+    syllablesContainer.innerHTML = '';
 
-    syllablesContainer.innerHTML = ''; // Limpa o contêiner de sílabas
+    let syllablesToDisplay = currentWord.syllables.slice();
+    if (isPhaseTwo) {
+        shuffleArray(syllablesToDisplay);
+    }
 
-    // Para cada sílaba da palavra atual
-    currentWord.syllables.forEach(syllable => {
+    syllablesToDisplay.forEach(syllable => {
         const syllablesWithVowels = generateSyllablesWithVowels(syllable);
-
-        // Para cada sílaba com vogais gerada
         syllablesWithVowels.forEach(syllableWithVowel => {
-            const syllableElement = document.createElement('div'); // Cria um elemento para a sílaba
-            syllableElement.classList.add('syllable'); // Adiciona a classe 'syllable' para estilização
-            syllableElement.textContent = syllableWithVowel; // Define o texto da sílaba
-            // Adiciona um evento de clique para selecionar/desselecionar a sílaba
-            syllableElement.addEventListener('click', () => toggleSyllable(syllableWithVowel, syllableElement));
-            syllablesContainer.appendChild(syllableElement); // Adiciona a sílaba ao contêiner
+            const syllableElement = document.createElement('div');
+            syllableElement.classList.add('syllable');
+            syllableElement.textContent = syllableWithVowel;
+            syllableElement.addEventListener('click', () => toggleSyllable(syllableWithVowel.toUpperCase(), syllableElement));
+            syllablesContainer.appendChild(syllableElement);
         });
     });
+
+    if (isPhaseTwo) {
+        startTimer();
+    }
 }
 
-// Função para selecionar/desselecionar uma sílaba
 function toggleSyllable(syllable, element) {
     const index = selectedSyllables.indexOf(syllable);
-    // Se a sílaba não estiver selecionada, adiciona-a ao array e destaca-a
     if (index === -1) {
         selectedSyllables.push(syllable);
         element.classList.add('selected');
-    } else { // Caso contrário, remove-a do array e remove o destaque
+    } else {
         selectedSyllables.splice(index, 1);
         element.classList.remove('selected');
     }
-    
-    updateSelectedSyllablesDisplay(); // Atualiza a exibição das sílabas selecionadas
+
+    updateSelectedSyllablesDisplay();
 }
 
-// Função para verificar a palavra formada pelo usuário
 function checkWord() {
-    const sortedSelectedSyllables = selectedSyllables.sort().join('');
-    const sortedWordSyllables = currentWord.syllables.sort().join('');
+    const sortedSelectedSyllables = selectedSyllables.map(s => s.toUpperCase()).sort().join('');
+    const sortedWordSyllables = currentWord.syllables.map(s => s.toUpperCase()).sort().join('');
 
-    // Se as sílabas selecionadas coincidirem com as da palavra atual
     if (sortedSelectedSyllables === sortedWordSyllables) {
-        // Exibe uma mensagem de parabéns com a imagem de sucesso
         Swal.fire({
             title: 'Parabéns!',
-            text: 'Você acertou a palavra.',
-            imageUrl: 'certo.gif', // imagem de sucesso
+            text: `Você acertou a palavra: ${currentWord.syllables.join('')}`,
+            imageUrl: 'certo.gif',
             imageWidth: 500,
             imageHeight: 280,
             imageAlt: 'Imagem de sucesso',
+            confirmButtonColor: '#28a745'
         });
-        correctCount++; // Incrementa o contador de acertos
-        correctCountSpan.textContent = correctCount; // Atualiza o contador de acertos no HTML
-        nextWord(); // Passa para a próxima palavra
+        correctCount++;
+        correctCountSpan.textContent = correctCount;
+        nextWord();
     } else {
-        // Se as sílabas selecionadas não coincidirem com as da palavra atual, exibe uma mensagem de erro
         Swal.fire({
             title: 'Tente novamente',
             text: 'Você errou a palavra.',
-            imageUrl: 'erro.gif', //  imagem de erro
+            imageUrl: 'erro.gif',
             imageWidth: 480,
             imageHeight: 360,
             imageAlt: 'Imagem de erro',
+            confirmButtonColor: '#28a745'
         });
-        wrongCount++; // Incrementa o contador de erros
-        wrongCountSpan.textContent = wrongCount; // Atualiza o contador de erros no HTML
+        wrongCount++;
+        wrongCountSpan.textContent = wrongCount;
     }
 
-    selectedSyllables = []; // Limpa as sílabas selecionadas
-    updateSelectedSyllablesDisplay(); // Atualiza a exibição das sílabas selecionadas
-    checkIfGameEnded(); // Verifica se o jogo terminou
+    clearSelectedSyllables();
+    checkIfGameEnded();
 }
 
-// Função para verificar se o jogo terminou
 function checkIfGameEnded() {
-    if (correctCount + wrongCount === words.length) {
-        // Exibe o nome, idade e pontuação do usuário
+    const totalAttempts = correctCount + wrongCount;
+
+    if (totalAttempts === 10) {
         Swal.fire({
-            title: 'Fim do Jogo',
-            html: `
-                <p>Nome: ${userName}</p>
-                <p>Idade: ${userAge}</p>
-                <p>Acertos: ${correctCount}</p>
-                <p>Erros: ${wrongCount}</p>
-            `,
-            confirmButtonText: 'Jogar Novamente'
-        }).then(() => {
-            // Reinicia o jogo
-            correctCount = 0;
-            wrongCount = 0;
-            correctCountSpan.textContent = correctCount;
-            wrongCountSpan.textContent = wrongCount;
-            currentWordIndex = 0;
-            currentWord = words[currentWordIndex];
-            displayWord();
+            title: 'Fim da Fase 1',
+            text: 'Você deseja continuar para a Fase 2?',
+            confirmButtonText: 'Sim',
+            cancelButtonText: 'Não',
+            showCancelButton: true,
+            confirmButtonColor: '#28a745'
+        }).then((result) => {
+            if (result.isConfirmed) {
+                isPhaseTwo = true;
+                correctCount = 0;
+                wrongCount = 0;
+                correctCountSpan.textContent = correctCount;
+                wrongCountSpan.textContent = wrongCount;
+                currentWordIndex = 0;
+                shuffleArray(wordsPhaseTwo);
+                currentWord = wordsPhaseTwo[currentWordIndex];
+                displayWord();
+            } else {
+                Swal.fire({
+                    title: 'Fim do Jogo',
+                    html: `
+                        <p>Nome: ${userName}</p>
+                        <p>Idade: ${userAge}</p>
+                        <p>Acertos: ${correctCount}</p>
+                        <p>Erros: ${wrongCount}</p>
+                    `,
+                    confirmButtonText: 'Jogar Novamente',
+                    confirmButtonColor: '#28a745'
+                }).then(() => {
+                    resetGame();
+                });
+            }
         });
     }
 }
 
-// Função para atualizar a exibição das sílabas selecionadas
 function updateSelectedSyllablesDisplay() {
-    selectedSyllablesDiv.textContent = ' ' + selectedSyllables.join(', '); // Atualiza o texto com as sílabas selecionadas
+    selectedSyllablesDiv.textContent = ' ' + selectedSyllables.join(', ');
 }
 
-// Função para corrigir a última sílaba selecionada
 function correctSyllable() {
-    selectedSyllables.pop(); // Remove a última sílaba do array de sílabas selecionadas
-    updateSelectedSyllablesDisplay(); // Atualiza a exibição das sílabas selecionadas
+    selectedSyllables.pop();
+    updateSelectedSyllablesDisplay();
+    clearSelectedSyllables();
 }
 
-// Função para passar para a próxima palavra do jogo
 function nextWord() {
-    currentWordIndex = (currentWordIndex + 1) % words.length; // Calcula o índice da próxima palavra
-    currentWord = words[currentWordIndex]; // Define a próxima palavra
-    displayWord(); // Exibe a próxima palavra
+    currentWordIndex = (currentWordIndex + 1) % (isPhaseTwo ? wordsPhaseTwo.length : words.length);
+    currentWord = isPhaseTwo ? wordsPhaseTwo[currentWordIndex] : words[currentWordIndex];
+    clearSelectedSyllables();
+    displayWord();
 }
 
-// Função para pedir nome e idade antes de iniciar o jogo
-function askNameAndAge() {
-    Swal.fire({
-        
-        title:'Jogo de Silabas',      
-        html: `
-            <label for="name">Nome:</label>
-            <input type="text" id="name" class="swal2-input" placeholder="Digite seu nome">
-            <label for="age">Idade:</label>
-            <input type="number" id="age" class="swal2-input" placeholder="Digite sua idade">
-        `,
-        focusConfirm: false,
-        preConfirm: () => {
-            const name = Swal.getPopup().querySelector('#name').value;
-            const age = Swal.getPopup().querySelector('#age').value;
-            if (!name || !age) {
-                Swal.showValidationMessage('Por favor, digite seu nome e idade');
-                return false;
-            }
-            return { name, age };
-        }
-    }).then((result) => {
-        if (result.isConfirmed) {
-            // Armazena os dados do usuário
-            userName = result.value.name;
-            userAge = result.value.age;
+function previousWord() {
+    currentWordIndex = currentWordIndex === 0 ? (isPhaseTwo ? wordsPhaseTwo.length - 1 : words.length - 1) : currentWordIndex - 1;
+    currentWord = isPhaseTwo ? wordsPhaseTwo[currentWordIndex] : words[currentWordIndex];
+    clearSelectedSyllables();
+    displayWord();
+}
 
-            // Exibe uma mensagem de boas-vindas
-            Swal.fire(`Olá, ${userName}! Vamos começar o jogo.`).then(() => {
-                // Inicia o jogo após a mensagem de boas-vindas
-                displayWord();
-            });
-        }
+function clearSelectedSyllables() {
+    selectedSyllables = [];
+    updateSelectedSyllablesDisplay();
+    const selectedElements = document.querySelectorAll('.syllable.selected');
+    selectedElements.forEach(element => {
+        element.classList.remove('selected');
     });
 }
 
-// Pede o nome e a idade ao carregar a página
+function startTimer() {
+    clearInterval(timer);
+    let timeLeft = timeLimit;
+    timer = setInterval(() => {
+        if (timeLeft <= 0) {
+            clearInterval(timer);
+            checkWord();
+        } else {
+            timeLeft--;
+            document.getElementById('timer').textContent = `Tempo restante: ${timeLeft}s`;
+        }
+    }, 1000);
+}
+
+function askNameAndAge() {
+    Swal.fire({
+        title: 'insira seu nome !',
+        html: `
+            <label for="name">Nome:</label>
+            <input type="text" id="name" class="swal2-input">
+            
+        `,
+        confirmButtonText: 'Iniciar Jogo',
+        confirmButtonColor: '#28a745',
+        preConfirm: () => {
+            const name = Swal.getPopup().querySelector('#name').value;
+            
+            if (!name ) {
+                Swal.showValidationMessage('Por favor, insira nome ');
+            }
+            return { name,  };
+        }
+    }).then((result) => {
+        userName = result.value.name;
+        userAge = result.value.age;
+        startGame();
+    });
+}
+
+function startGame() {
+    correctCount = 0;
+    wrongCount = 0;
+    correctCountSpan.textContent = correctCount;
+    wrongCountSpan.textContent = wrongCount;
+    isPhaseTwo = false;
+    shuffleArray(words);
+    currentWordIndex = 0;
+    currentWord = words[currentWordIndex];
+    displayWord();
+}
+
+function resetGame() {
+    correctCount = 0;
+    wrongCount = 0;
+    correctCountSpan.textContent = correctCount;
+    wrongCountSpan.textContent = wrongCount;
+    isPhaseTwo = false;
+    askNameAndAge();
+}
+
+// Inicie o jogo ao carregar a página
 askNameAndAge();
